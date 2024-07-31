71273c016d5d8e2aa256f7ce26159599dca9d540
kurbiself <kurbiself@mail.ru>
2024-07-31 17:27:43 +0700

    Add HW №5 of module 2

---
+++ module_2_5.py
@@ -0,0 +1,27 @@
+def get_matrix(n: int, m: int, value) -> list:
+    """Create a matrix of n x m
+
+    Args:
+        n: line size
+        m: column size
+        value: matrix value
+
+    Returns:
+        matrix
+
+    """
+    matrix = []
+    for i in range(n):
+        list_for_matrix = []
+        for j in range(m):
+            list_for_matrix.append(value)
+        matrix.append(list_for_matrix)
+    return matrix
+
+
+result1 = get_matrix(0, 2, 10)
+result2 = get_matrix(3, 5, 42)
+result3 = get_matrix(4, 2, 13)
+print(result1)
+print(result2)
+print(result3)
