4693b12a09fd3b77208b12fb0a97de8cee754138
kurbiself <kurbiself@mail.ru>
2024-08-13 22:02:07 +0700

    Add directories

--- module4/module_4_1.py
+++ module4/module_4_1.py
@@ -1,7 +1,7 @@
-from .fake_math import divide as fake_divide
-from . import true_math
+from fake_math import divide as fake_divide
+from true_math import divide as true_divide
 
 print(fake_divide(69,3))
 print(fake_divide(3,0))
-print(true_math.divide(49, 7))
-print(true_math.divide(15, 0))
+print(true_divide(49, 7))
+print(true_divide(15, 0))
