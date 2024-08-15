4100d9d5745608bb34efddb65591c6305d13a5e5
kurbiself <kurbiself@mail.ru>
2024-08-15 18:13:52 +0700

    Add Practical task

---
+++ module4/sortfunc.py
@@ -0,0 +1,24 @@
+nums = [11, 45, 3, 2, 7, 6, 27]
+
+
+def bubble_sort(ls: list):
+    swapped = True
+    while swapped:
+        swapped = False
+        for i in range(len(ls) - 1):
+            if ls[i] > ls[i + 1]:
+                ls[i], ls[i + 1] = ls[i + 1], ls[i]
+                swapped = True
+
+
+def selection_sort(ls: list):
+    for i in range(len(ls)):
+        lowest = i
+        for j in range(i + 1, len(ls)):
+            if ls[j] < ls[lowest]:
+                lowest = j
+        ls[i], ls[lowest] = ls[lowest], ls[i]
+
+
+bubble_sort(nums)
+print(nums)
