cb74c32d729c09f8ebb29998a157116cee8497ac
kurbiself <kurbiself@mail.ru>
2024-12-26 23:26:44 +0700

    Add HW 1 from module 17

---
+++ module17/app/routers/task.py
@@ -0,0 +1,28 @@
+from fastapi import APIRouter
+
+router = APIRouter(prefix='/task', tags=['task'])
+
+
+@router.get('/all_task')
+async def all_tasks():
+    pass
+
+
+@router.get('/task_id')
+async def task_by_id():
+    pass
+
+
+@router.post('/create')
+async def create_task():
+    pass
+
+
+@router.put('/update_task')
+async def update_task():
+    pass
+
+
+@router.delete('/delete')
+async def delete_task():
+    pass
